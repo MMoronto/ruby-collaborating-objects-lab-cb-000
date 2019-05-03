af6ecb3175271be82ac9d63fe8d268245b4d4f6a
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-05-03 18:01:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,7 +21,7 @@ class Artist
   end 
   
   def self.find_or_create_by_name(name)
-      self.find(name) || self.new(name)
+      self.find(name) || self.create(name)
   end 
   
   def add_song(song)
