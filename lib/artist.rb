68ec674543b9e976d7a159dc1ca674b7903d35e6
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-05-03 18:06:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,11 +17,15 @@ class Artist
   end 
   
   def self.find(name)
-      self.all.detect{|artist| artist.name == name}
+      self.all.find{|artist| artist.name == name}
+  end 
+  
+  def self.create(name)
+      self.new(name).tap {|artist| artist.save}
   end 
   
   def self.find_or_create_by_name(name)
-      self.find(name) || self.create(name)
+      self.find(name) ? self.find(name) : self.create(name)
   end 
   
   def add_song(song)
